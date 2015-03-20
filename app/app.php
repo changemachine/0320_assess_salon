47a9ad12fe1295d5256dd92fad2bb97b3fa21555
James Nielson <james@changemachine.org>
2015-03-20 16:59:43 -0700

    Add a little twig to get a home page going.

--- app/app.php
+++ app/app.php
@@ -18,6 +18,21 @@
         return $app['twig']->render('index.twig', array('clients' => Client::getAll()));
     });
 
+        // Add client, display all on the home page
+    $app->post('/', function() use($app){
+        $id = $_POST['id'];
+        $client_name = $_POST['client_name'];
+        $client = new Client($id, $client_name);
+        $client->save();
+        return $app['twig']->render('index.twig', array('clients' => Client::getAll()));
+    });
+
+
+    //
+    // $app->get('/clients', function() use($app){
+    //         return $app['twig']->render('clients.twig', array('clients' =>))
+    // });
+
     return $app;
 
 
